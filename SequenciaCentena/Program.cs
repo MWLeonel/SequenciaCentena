5b4cf442212ec26ffe3149f54c3ceced23030aed
MWLeonel <wmleonel6@gmail.com>
2020-10-13 12:05:29 -0300

    Exibir numeros de 0 a 100

---
+++ SequenciaCentena/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SequenciaCentena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1;
+
+            while (numero <= 100)
+            {
+                Console.Write(numero);
+                Console.Write(" ");
+                numero += 1;
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+        }
+    }
+}
